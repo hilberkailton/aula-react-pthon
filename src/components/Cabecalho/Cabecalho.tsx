eebd12f9d66ca4fbd0ebafb5252ad4dbb7e21aba
Hilker Kaiko <hylber.kailton2014@gmail.com>
2022-10-10 09:42:48 -0300

    React e Next.js

---
+++ src/components/Cabecalho/Cabecalho.tsx
@@ -0,0 +1,13 @@
+import { Cabecalhocontainer, Logo } from "./cabecalho.style";
+
+const Cabecalho = () => {
+    return (
+        <Cabecalhocontainer>
+             <div>
+                 <Logo src="/images/myteacher.png"/>
+                     </div>
+                     <p>Encontre o professor perfeito!</p>
+        </Cabecalhocontainer>
+    )
+}
+export default Cabecalho;
---
+++ src/components/Lista/Lista.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@mui/material";
+import { Professor } from "../../@types/professor";
+import { Descricao, Foto, Informacoes, ItemLista, ListaStyled, ListaVazia, Nome, Valor } from "./Lista.style";
+
+interface listaProps{
+    professores: Professor[],
+}
+
+const Lista =(props: listaProps)=> {
+return (
+    <div>
+        {props.professores.length > 0 ? (
+             <ListaStyled>
+      
+             {props.professores.map(professor =>(
+               <ItemLista key={professor.id}>
+                <Foto src={professor.foto}></Foto>
+                  <Informacoes>
+                     <Nome>{professor.nome}</Nome>
+                     <Valor>{professor.valor_hora.toLocaleString('pt-BR',{minimumFractionDigits:2, style: 'currency' ,currency: 'BRl'})}</Valor>
+                     <Descricao>{professor.descricao}</Descricao>
+                     <Button sx={{width: '70%'}}>Marcar Aula com hilber</Button>
+                  </Informacoes>
+               </ItemLista>
+             ))}    
+           </ListaStyled>
+        ) : (
+          <ListaVazia>Nenhum item encontrado</ListaVazia>
+        ) }
+    </div>
+)
+}
+export default Lista;
